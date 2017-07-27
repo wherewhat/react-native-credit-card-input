d82d7e660979a09366a3957fa5ec3465dc4fc5f9
Hector Aristy <hectoraristy@Hectors-MacBook-Pro.local>
2017-07-27 16:30:10 -0400

    remove width and height from image-constants.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,13 @@
+import React, { Component, PropTypes } from "react";
+import ReactNative, {
+  NativeModules,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+
 const { width, height } = Dimensions.get('window');
 const Constants = {
   SCREEN_SIZE: {
